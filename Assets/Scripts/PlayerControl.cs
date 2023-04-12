c672e6be78864fa74c1a6247b4870901de5ebf08
goliath92 <efeeray92@gmail.com>
2023-04-12 22:26:02 +0300

    MovementTest (Not working properly)

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,63 +9,97 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    private ColorCheckScript _colorCheckScript;
-    
-    private Rigidbody rb;
-    
-    public float playerSpeed = 10;
-    
-    public float rotateSpeed = 2;
-    
-    private float HorizontalValue;
-    
-    public Color ringColor;
+    private CharacterController controller;
 
+    public float forwardSpeed;
+    private Vector3 direction;
+    private Vector3 targetPosition;
+
+    private int desiredLane = 1;                 // 0:Left 1:Middle 2:Right
+    public float laneDistance = 4;               // distance between two lanes
     
-    
+
+    private ColorCheckScript _colorCheckScript;
+
+    public Color ringColor;                                // ??????????????????????????????
+
+
+
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
 
         _colorCheckScript = GetComponent<ColorCheckScript>();
     }
 
     private void Update()
     {
-        HorizontalValue = Input.GetAxis("Horizontal");           // keyboard input
+        direction.z = forwardSpeed;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if (desiredLane == 3)                      // to prevent going outside the bounder
+                desiredLane = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+                desiredLane = 0;
+        }
+
+        targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        
+
+        if (desiredLane == 0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+            
+        }
+
+        else if(desiredLane == 2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+            
+        }
+
+        transform.position = Vector3.Lerp(transform.position,targetPosition, 80 * Time.fixedDeltaTime);
+        
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        rb.AddForce(new Vector3(HorizontalValue * rotateSpeed, 0, 1) * playerSpeed);                 // player movement
+        controller.Move(direction * Time.fixedDeltaTime);
+        
     }
 
-    
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Finish"))                    // stop when reaching the finish line
         {
-            playerSpeed = 0;
-            rotateSpeed = 0;
+          
             other.gameObject.SetActive(false);
         }
  
         if (other.gameObject.CompareTag("RedRing"))             // change color when pass a ring
         {
             
-            ringColor = Color.red;                           // to compare player's and rings colors
+            ringColor = Color.red;                           // ??????????????????????????????
             _colorCheckScript.checkColor();                  // check if player enters wrong color
             
         }
         if (other.gameObject.CompareTag("BlueRing"))
         {      
-            ringColor = Color.blue;
+            ringColor = Color.blue;                         // ??????????????????????????????
             _colorCheckScript.checkColor();
             
         }
         if (other.gameObject.CompareTag("YellowRing"))
         {
-            ringColor = Color.yellow;
+            ringColor = Color.yellow;                          // ??????????????????????????????
             _colorCheckScript.checkColor();
             
         }
