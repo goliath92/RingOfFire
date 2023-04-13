03daa8a4c6ec8e7cc982e08ec7f481149be73dac
goliath92 <efeeray92@gmail.com>
2023-04-13 20:57:56 +0300

    MovementFixed
    - 3 lane movement works properly, will test if any smoothing required
    - additionally working on camera movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -11,16 +11,18 @@ public class PlayerControl : MonoBehaviour
 {
     private CharacterController controller;
 
-    public float forwardSpeed;
-    private Vector3 direction;
-    private Vector3 targetPosition;
-
-    private int desiredLane = 1;                 // 0:Left 1:Middle 2:Right
-    public float laneDistance = 4;               // distance between two lanes
     
 
-    private ColorCheckScript _colorCheckScript;
+    //Movement
+    private float jumpForce = 4.0f;
+    private float gravity = 12.0f;
+    private float verticalVelocity;
+    private float speed = 7.0f;
+    private int desiredLane = 1;   //0:Left 1:middle 2:Right
+    public float laneDistance = 3.0f;
 
+    //Color
+    private ColorCheckScript _colorCheckScript;
     public Color ringColor;                                // ??????????????????????????????
 
 
@@ -30,50 +32,49 @@ public class PlayerControl : MonoBehaviour
         controller = GetComponent<CharacterController>();
 
         _colorCheckScript = GetComponent<ColorCheckScript>();
+
+        
     }
 
     private void Update()
     {
-        direction.z = forwardSpeed;
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            desiredLane++;
-            if (desiredLane == 3)                      // to prevent going outside the bounder
-                desiredLane = 2;
-        }
-
+        // gather the inputs on which lane we should be
         if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            desiredLane--;
-            if (desiredLane == -1)
-                desiredLane = 0;
-        }
+            MoveLane(false);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            MoveLane(true);
 
-        targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
-        
+        // Calculate where we should be in the future
+        Vector3 targetPosition = transform.position.z * Vector3.forward;             // forward is for we always wanna go forward
 
         if (desiredLane == 0)
-        {
             targetPosition += Vector3.left * laneDistance;
-            
-        }
-
-        else if(desiredLane == 2)
-        {
+        else if (desiredLane == 2)
             targetPosition += Vector3.right * laneDistance;
-            
-        }
 
-        transform.position = Vector3.Lerp(transform.position,targetPosition, 80 * Time.fixedDeltaTime);
-        
+        // calculate move delta
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = (targetPosition - transform.position).normalized.x * speed;
+        moveVector.y = -0.1f;
+        moveVector.z = speed;
+
+        //Move the Player
+        controller.Move(moveVector * Time.deltaTime);
     }
 
     private void FixedUpdate()
     {
-        controller.Move(direction * Time.fixedDeltaTime);
+       
         
     }
+
+    private void MoveLane(bool goingRight)
+    {
+        desiredLane += (goingRight) ? 1 : -1;              // ? is a short way for if else logic (if 1 else -1 in here)
+        desiredLane = Mathf.Clamp(desiredLane, 0, 2);
+
+    }
+    
 
 
     private void OnTriggerEnter(Collider other)
