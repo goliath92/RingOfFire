b7d5000975d48218ea1ef569b3f412121891663a
goliath92 <efeeray92@gmail.com>
2023-04-13 21:36:28 +0300

    Camera movement added
    - Camera movement added
    - CharController radius arranged

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -6,7 +6,12 @@ public class CameraScript : MonoBehaviour
 {
     public Transform target;    // player
     public Vector3 offset = new Vector3(0, 5.0f, -10f);
-    
+
+    private void Start()
+    {
+        transform.position = target.position + offset;
+    }
+
     private void LateUpdate()
     {
         // camera's position according to the target
