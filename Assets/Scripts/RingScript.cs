c900d6322e6cd269fca68443f6ba75dde58bdea0
goliath92 <efeeray92@gmail.com>
2023-05-10 22:57:09 +0300

    Sound and Menu
    Ring collect sound implemented
    - working on main menu

--- Assets/Scripts/RingScript.cs
+++ Assets/Scripts/RingScript.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class RingScript : MonoBehaviour
 {
+    public AudioClip pickupSound;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
+        {
+            //play pickup sound
+            AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+            //disable the object
             this.gameObject.SetActive(false);
-
+        }
+            
     }
 }
